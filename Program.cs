ef0dd879df8b239097944b4e5a2a8477d8096bbd
todorasenov <todorsnv@gmail.com>
2023-06-19 10:22:44 +0300

    tt

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,20 +10,29 @@ namespace TelephoneExchange
 	public class Program
 	{
 		static void Main(string[] args)
+
 		{
 			try
 			{
+			StreamWriter streamWriter = new StreamWriter("project.txt");
 				int count = int.Parse(Console.ReadLine());
-				List<Danni> list = new List<Danni>();
+				List<Profession> list = new List<Profession>();
 				for (int i = 0; i < count; i++)
 				{
 					var danni = Console.ReadLine().Split();
-					Danni profession = new Profession(int.Parse(danni[0]), danni[1], int.Parse(danni[2]), danni[3], danni[4]);
+					Profession profession = new Profession(int.Parse(danni[0]), danni[1], danni[2],int.Parse( danni[3]), danni[4]);
 					list.Add(profession);
+					using (streamWriter)
+					{
+						streamWriter.Write(danni[0]);
+						streamWriter.Write(danni[1]);
+						streamWriter.Write(danni[2]);
+						streamWriter.Write(danni[3]);
+					}
 				}
 
 				list.Sort();
-				Console.WriteLine(list.Select(x => x.Age).Min());
+				Console.WriteLine(list.Select(x =>x.Age).Min());
 				list.ForEach(x => x.Print());
 			}
 			catch (ArgumentException exception)
@@ -30,6 +40,9 @@ namespace TelephoneExchange
 				Console.WriteLine($"ArgException: {exception.Message}");
 				throw;
 			}
+			
+
+
 		}
 	}
 }
